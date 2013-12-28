d82fea69e1f554876dd582a8bfdcc4a94c93acac
Alex Willemsma <alex@sovovle.com>
2013-12-27 16:15:46 -1000

    Minor tweaks, updated attributes file.

--- site-cookbooks/panomira_rails_api/attributes/default.rb
+++ site-cookbooks/panomira_rails_api/attributes/default.rb
@@ -6,11 +6,11 @@ default["rails_api"].tap do |rails_api|
   rails_api["group"] = "deploy"
   rails_api["subdomain"] = "api"
 
-  rails_api["repo"] = "git@github.com:sovolve/panomira-api.git"
+  rails_api["repo"] = "git@github.com:sovolve/panomira-api-ror.git"
 
   rails_api["database_name"] = "panomira_rails_api"
   rails_api["database_username"] = "mysql_rails_api"
-  rails_api["database_password"] = "secReTPW@All"
+  rails_api["database_password"] = "anotherP@ssword"
 
   rails_api["neo4j_main"]["port"] = 7476
   rails_api["neo4j_contacts"]["port"] = 7477
